792e4e91dc4d7fb7f5e1fe47014f09021db53e86
Jhon <jhonlly_blim_blim@hotmail.com>
2017-02-13 18:50:20 +0000

    Ejercicio finalizado

---
+++ Preferecias.java
@@ -0,0 +1,10 @@
+package com.majada.jhona.practica3_1;
+
+import android.preference.PreferenceActivity;
+
+/**
+ * Created by jhona on 31/01/2017.
+ */
+
+public class Preferecias extends PreferenceActivity {
+}
